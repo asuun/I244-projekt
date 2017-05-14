58a82ee8d0b54f6e3922a23add9918bad1a8ea6c
asuun <alvarsuun@gmail.com>
2017-05-14 21:37:23 +0300

    PHP+ SQL VOL 2

--- login.php
+++ login.php
@@ -3,10 +3,8 @@ $servername = "localhost";
 $username = "test";
 $password = "t3st3r123";
 $database = "test";
-$eesnimi= $_REQUEST["eesnimi"];
-$perenimi= $_GET["perenimi"];
-$tase=$_GET["tase"];
-// Create connection
+
+// andmebaasi sisselogimine
 $conn = new mysqli($servername, $username, $password, $database);
 // Check connection
 if ($conn->connect_error) {
@@ -14,15 +12,16 @@ if ($conn->connect_error) {
 }
 
 
-$sql = "SELECT eesnimi FROM 000I244tabelkatse";
+$sql = "SELECT eesnimi,perenimi FROM 000I244tabelkatse WHERE eesnimi=$eesnimi";
 
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    echo "<table><tr><th>eesnimi</th><th>perenimi</th><th>aeg</th><th>ID</th></tr><table>";
     // output data of each row
+
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["eesnimi"]."</td><td>".$row["perenimi"]." ".$row["aeg"]."</td></tr>";
+        echo "<tr><td>".$row["eesnimi"]."</td><td>".$row["perenimi"]."</td></tr>";
     }
     echo "</table>";
 } else {
